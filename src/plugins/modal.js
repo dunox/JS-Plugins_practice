4819fe1df4bf8d14c62fb85ffa65bc837974617e
Andriy <34597632+dunox@users.noreply.github.com>
2021-03-10 19:07:48 +0200

    modal to js file

--- src/plugins/modal.js
+++ src/plugins/modal.js
@@ -1,5 +1,35 @@
 import $ from "jQuery";
+
+const _createModal = (options) => {
+  const modal = document.createElement("div");
+  modal.classList.add("amodal");
+  modal.insertAdjacentElement(
+    "afterbegin",
+    `
+    <div class="modal-overlay">
+        <div class="modal-window">
+          <div class="modal-header">
+            <span class="modal-title">Modal Title</span>
+            <span class="modal-close">&times;</span>
+          </div>
+          <div class="modal-body">
+            <p>Lorem ipsum dolor sit.</p>
+            <p>Lorem ipsum dolor sit.</p>
+          </div>
+          <div class="modal-footer">
+            <button>Ok</button>
+            <button>Cancel</button>
+          </div>
+        </div>
+      </div>
+  `
+  );
+  document.body.appendChild(modal);
+  return modal;
+};
+
 $.modal = (options) => {
+  const $modal = _createModal(options);
   return {
     open() {},
     close() {},
